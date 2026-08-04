087d64076db0edfd94ace30846dd1c125b2f2763
Punyawee <punyawee.miu@gmail.com>
2026-08-04 11:05:23 +0700

    task: add authorize button

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,7 @@ async function bootstrap() {
     .setTitle('Simple Backend API')
     .setDescription('API documentation')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
